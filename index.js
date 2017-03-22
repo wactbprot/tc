129c1e2dd4cde0c60ce07bdc06bad96a45962365
wactbprot <thsteinbock@web.de>
2017-03-22 14:24:37 +0100

    ec[v.eadr[i]][v.eclosed[i]]

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var con = { method: 'POST',
             json: true,
             body: {
               "Skip": 1,
-              "Quantity": 20,
+              "Quantity": 8,
               "OutMode": "8Bits*",
               "Host": "172.30.56.46",
               "Action": "MODBUS"
@@ -44,30 +44,39 @@ var count = (function(){
              })();
 
 var state = (function(){
-               var es
-                 , vs;
+               var ec = {}
+                 , eo = {}
+                 , vs = {};
                return {
-                 eGet:function(){
-                   return es;
+                 ecGet:function(adr){
+                   return ec[adr];
                  },
-                 eSet:function(s){
-                   es = s;
+                 eoGet:function(adr){
+                   return eo[adr];
                  },
-                 vGet:function(){
-                   return vs;
+                 ecSet:function(s, adr){
+                   ec[adr] = s;
+
                  },
-                 vSet:function(s){
-                   vs = s;
+                 eoSet:function(s, adr){
+                   eo[adr] = s;
+
+                 },
+                 vGet:function(adr){
+                   return vs[adr];
+                 },
+                 vSet:function(s, adr){
+                   vs[adr] = s;
                  },
                  draw:function(v, i){
                    var nd  = (new Date()).toTimeString().split(" ")[0]
-                   if(es && vs && v.box && v.box[i]){
-                     var ecl = es[v.eopen[i]] ? 'closed-switch: off' : 'closed-switch: on'
-                       , eop = es[v.eclosed[i]] ? 'open-switch: off'   : 'open-switch: on'
+                   if(ec && eo && vs && v.box && v.box[i]){
+                     var ecl = ec[v.eadr[i]][v.eclosed[i]] ? 'closed-switch: off' : 'closed-switch: on'
+                       , eop = eo[v.eadr[i]][v.eopen[i]] ? 'open-switch: off'   : 'open-switch: on'
                        , ostr = '{green-fg}' + v.name[i] +'\n\nopen\n'   + ecl + '\n' + eop + '\n{/green-fg}'
                        , cstr = '{red-fg}'   + v.name[i] +'\n\nclosed\n' + ecl + '\n' + eop + '\n{/red-fg}'
 
-                     var vstr = vs[v.vpos[i]] ? ostr : cstr;
+                     var vstr = vs[v.vadr[i]][v.vpos[i]] ? ostr : cstr;
                      v.box[i].setContent( vstr  + nd);
                      screen.render();
                    }
@@ -91,9 +100,9 @@ var valve = {
       bg: 'black'
     }
   },
-  "vadr": ["45407","45407","45407","45407","45407","45407","45407","45407","45407","45407","45407","45407"],
-  "eadr": ["45395","45395","45395","45395","45395","45395","45395","45395","45395","45395","45395","45395"],
-  "wadr": ["40003","40003","40003","40003","40003","40003","40003","40003","40003","40003","40003","40003"],
+  "vadr": ["45407","45407","45407","45407","45409","45409","45409","45409","45411","45411","45411","45411"],
+  "eadr": ["45395","45395","45395","45395","45397","45397","45397","45397","45399","45399","45399","45399"],
+  "wadr": ["40003","40003","40003","40003","40004","40004","40004","40004","40005","40005","40005","40005"],
   name:[ "V1 (shutter)"
        , "V2 (1T CDGs)"
        , "V3 (10T CDGs)"
@@ -108,9 +117,9 @@ var valve = {
        , "V12"],
   row     :[ 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3],
   col     :[ 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2],
-  vpos    :[ 0, 2, 4, 6, 8,10,12,14,16,16,16,16],
-  eopen   :[ 0, 2, 4, 6, 8,10,12,14,16,16,16,16],
-  eclosed :[ 1, 3, 5, 7, 9,11,13,15,17,17,17,17],
+  vpos    :[ 0, 2, 4, 6, 0, 2, 4, 6, 0, 2, 4, 6],
+  eopen   :[ 0, 2, 4, 6, 0, 2, 4, 6, 0, 2, 4, 6],
+  eclosed :[ 1, 3, 5, 7, 1, 3, 5, 7, 1, 3, 5, 7],
   ini:[],
   box:[]
 }
@@ -135,8 +144,11 @@ var ini = function(i){
 }
 
 var swtch =  function(j){
-  wcon.body.Address = valve.wadr[j];
-  var vc =  state.vGet();
+  var wadr = valve.wadr[j];
+  var vadr = valve.vadr[j];
+
+  wcon.body.Address = wadr ;
+  var vc =  state.vGet(vadr);
   if(_.isArray(vc)){
     vc[valve.vpos[j]] = vc[valve.vpos[j]] == 1 ? 0 : 1;
     wcon.body.Value = vc;
@@ -166,19 +178,23 @@ setInterval(function(){
     if(i == 11) first = false
   }
 
-  rcon.body.Address = valve.vadr[i];
+  var vadr = valve.vadr[i]
+  rcon.body.Address = vadr;
   request(rcon, function(error, response, body){
     if(error) {
       console.log(error);
     } else {
       if(body.Result){
-      state.vSet(body.Result);
-        rcon.body.Address = valve.eadr[i];
+      state.vSet(body.Result, vadr, i);
+        var eadr = valve.eadr[i]
+        rcon.body.Address = eadr;
+
         request(rcon, function(error, response, body){
           if(error) {
             console.log(error);
           } else {
-            state.eSet(body.Result);
+            state.ecSet(body.Result, eadr, i);
+            state.eoSet(body.Result, eadr, i);
             state.draw(valve, i);
 
           }
